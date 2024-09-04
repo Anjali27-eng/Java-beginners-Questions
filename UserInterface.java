9726344e10409c6b255c0fd149d4a8c8f3dae21a
Anjali Rajole <rajoleanjali04@gmail.com>
2024-09-04 20:27:29 +0530

    Add files via upload

--- UserInterface.java
+++ UserInterface.java
@@ -5,37 +5,23 @@ public class UserInterface {
 	public static void main(String[] args) 
 	{
 		Scanner sc = new Scanner(System.in);
-		//Fill the code here
-		System.out.println("Enter weight in kg");
-		double weight=sc.nextDouble();
 		
-		System.out.println("Enter height in cm");
-		double height=sc.nextDouble();
+		System.out.println("Enter the numbers");
+		int first = sc.nextInt();
+		int second = sc.nextInt();
 		
-		double height1=height/100;
+	    System.out.println("Before swapping");
+	    System.out.println("first = "+first+", second = "+second);
+	    //System.out.println();
+	    
+	    first =first ^ second;
+	    second =first ^second;
+	    first =first ^second;
+	    
+	    System.out.println("After swapping");
+	   System.out.println("first = "+first+", second = "+second);
+	   // System.out.println("second = "+second);
 		
-		double bmi =weight/(height1 * height1);
-		
-		System.out.printf("Your BMI is %.2f. ",bmi);
-		
-		if(bmi > 25){
-		System.out.println("You are overweight");
-		double reduce=bmi-25;
-		System.out.printf("Reduce %.2f kg to be fit",reduce);
-		}
-		else if(bmi < 18.5){
-		    System.out.println("You are underweight");
-		    //double min =18.5*height1*height1;
-		    double Gain = 18.5 - bmi;
-		    System.out.printf("Gain %.2f kg to be fit",Gain);
-		}
-		else if(bmi < 25 && bmi >=18.5){
-		     System.out.println("You are fit and healthy");
-		}
-		else{
-		    System.out.println("Invalid");
-		}
-		sc.close();
 	}
 
 }
